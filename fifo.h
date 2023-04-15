905fdf803a8ad56965af2d9d94c2476756a86237
Michael <michael.walton@g.austincc.edu>
2023-04-15 10:32:07 -0500

    defined Data and Node structs. wrote Queue class prototype

--- fifo.h
+++ fifo.h
@@ -1,11 +1,38 @@
-/*********************
-Name: Michael Walton
-COSC-2436-004
-Purpose:
- - Design FIFO Queue ADT
-**********************/
-
 #ifndef FIFO_H
 #define FIFO_H
+
+#include "fifo.h"
+#include <string>
+
+using std::string;
+
+//Standard Data Struct
+struct Data {
+    int id;
+    string data;
+};
+
+//Standard Node Struct
+struct Node {
+    Data data;
+    Node *next;
+};
+
+//Queue Class Prototype
+class Queue {
+
+public:
+    Queue();
+    ~Queue();
+
+    bool push(int, string*);
+    bool pull(Data&);
+    bool peek(Data&);
+    bool isEmpty();
+
+private:
+    Node *head;
+    Node *tail;
+};
 
 #endif //FIFO_H
